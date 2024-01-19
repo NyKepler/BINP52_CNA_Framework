d8696eac6b7a0ed42514b6372e48e47cb7fbf4ea
GuyuanTang <gu5747ta-s@student.lu.se>
2024-01-19 15:30:45 +0100

    Update the workflow
    Alter the logic of the Snakemake2

--- workflow/scripts/CN_sig.R
+++ workflow/scripts/CN_sig.R
@@ -10,6 +10,7 @@ library(NMF)
 library(flexmix)
 library(YAPSA)
 library(tidyverse)
+library(lsa)
 
 main_script = snakemake@input[['main']]
 helper_script = snakemake@input[['helper']]
@@ -22,14 +23,15 @@ source(helper_script)
 outdir <- snakemake@params[['outdir']]
 
 # extract the required values from the snakemake workflow
-groupID <- snakemake@params[['groupID']] # the group name
-binsize <- snakemake@params[['binsize']] # the binsize for the selected group
-sample_names <- snakemake@params[['sample_names']] # the list containing the sample names
+sample_info <- snakemake@params[['sample_info']] # the sample information tsv file
+sample_info <- read.table(sample_info, sep = '\t', header = 1)
+sample_names <- sample_info$Sample
 in_dir <- snakemake@params[['indir']] # the directory to store the segment files
 
 # generate the required input dataframes list
 segment_list <- list()
 for (i in sample_names) {
+  binsize <- sample_info[which(sample_info$Sample==i),'Binsize']
   tab_df <- read.table(paste0(in_dir, i, '/05_absolute_CN/', i, '_', binsize, 'kb_seg.tsv'), sep = '\t', header = 1) %>% select('chromosome','start','end','segVal')
   a <- length(segment_list) + 1
   segment_list[[a]] <- tab_df
@@ -39,23 +41,49 @@ names(segment_list) <- sample_names
 # validate the signatures in the samples
 CN_features <- extractCopynumberFeatures(segment_list)
 sample_by_component <- generateSampleByComponentMatrix(CN_features)
-output_matrix <- quantifySignatures(sample_by_component)
 
 # output the matrix files, matrix object(RDS) and a simple heatmap
-output_prefix <- paste0(outdir, groupID, '/CNsig/', groupID, '_CNsig.', binsize, 'kb')
-# matrix file
-write.table(output_matrix, file = paste0(output_prefix, '.matrix.txt'))
-# matrix object
-saveRDS(output_matrix, file = paste0(output_prefix, '.matrix.rds'), compress = FALSE)
-# simple heatmap
-pdf(paste0(output_prefix,'.heatmapSS.pdf'))
-heatmap(output_matrix)
-dev.off()
+output_prefix <- paste0(outdir,'CN_sig')
 # the same output for the sample-by-component matrix should also be saved for future analysis
 write.table(sample_by_component, file = paste0(output_prefix, '.SCmatrix.txt'))
 saveRDS(sample_by_component, file = paste0(output_prefix, '.SCmatrix.rds'), compress = FALSE)
 pdf(paste0(output_prefix, 'heatmapSC.pdf'))
 heatmap(sample_by_component)
 dev.off()
+
+
+# apply cosine similarity to find the enriched signature for each sample
+## load the signature definition table for Brenton's CN signatures
+feat_sig_mat <- snakemake@params[['def_SC']]
+CN_sig <- paste0('s',c(1:7))
+
+## load the Sample-by-Component matrix generated above
+SCmatrix <- t(sample_by_component)
+samples <- colnames(SCmatrix)
+
+## calculate the cosine similarity
+cos_mat <- cbind(feat_sig_mat, SCmatrix)
+cos_mat <- cosine(cos_mat)
+
+## extract the required signature similarity matrix
+SSmatrix <- cos_mat[samples,]
+SSmatrix <- as.data.frame(SSmatrix) %>% select(all_of(CN_sig))
+
+## find the most similar signature for each sample
+sample_by_signature <- apply(SSmatrix, 1, function(t) colnames(SSmatrix)[which.max(t)]) 
+sample_by_signature <- as.list(sample_by_signature)
+SSmatrix['enrich'] <- NA
+for (i in samples) {
+  sigID <- sample_by_signature[[i]]
+  SSmatrix[i, 'enrich'] <- sigID
+}
+
+## save the SSmatrix
+SSmatrix['sample'] <- rownames(SSmatrix)
+SSmatrix <- SSmatrix %>% select('sample', 'enrich', 's1':'s7')
+row.names(SSmatrix) <- 1:nrow(SSmatrix)
+write.table(SSmatrix, file = paste0(output_prefix,'.SSmatrix.tsv'), sep = '\t', row.names = FALSE, col.names = TRUE, quote = FALSE)
+saveRDS(SSmatrix, file = paste0(output_prefix,'.SSmatrix.rds'), compress = FALSE)
+
 
 
--- workflow/scripts/PanCan_sig.R
+++ workflow/scripts/PanCan_sig.R
@@ -8,14 +8,16 @@
 
 library(tidyverse)
 library(CINSignatureQuantification)
+library(lsa)
+library(openxlsx)
 
 # specify the output directory
 outdir <- snakemake@params[['outdir']]
 
 # extract the required values from the snakemake workflow
-groupID <- snakemake@params[['groupID']] # the group name
-binsize <- snakemake@params[['binsize']] # the binsize for the selected group
-sample_names <- snakemake@params[['sample_names']] # the list containing the sample names
+sample_info <- snakemake@params[['sample_info']] # the sample information tsv file
+sample_info <- read.table(sample_info, sep = '\t', header = 1)
+sample_names <- sample_info$Sample
 in_dir <- snakemake@params[['indir']] # the directory to store the segment files
 
 # generate the required sample table
@@ -23,6 +25,7 @@ sample_df <- data.frame(matrix(ncol = 5, nrow = 0))
 colnames(sample_df) <- c('chromosome', 'start', 'end', 'segVal', 'sample')
 ## fill in the sample information
 for (i in sample_names) {
+  binsize <- sample_info[which(sample_info$Sample==i), 'Binsize']
   tab_df <- read.table(paste0(in_dir, i, '/05_absolute_CN/', i, '_', binsize, 'kb_seg.tsv'), sep = '\t', header = 1) %>% select('chromosome','start','end','segVal')
   tab_df['sample'] <- i
   ### append the table to the final sample table
@@ -33,11 +36,11 @@ for (i in sample_names) {
 PanCan_sig <- quantifyCNSignatures(sample_df)
 
 # output the matrix files, matrix object(RDS) and a simple heatmap
-output_prefix <- paste0(outdir, groupID, '/PanSig/', groupID, '_PanSig.', binsize, 'kb')
+output_prefix <- paste0(outdir,'PanCan_sig')
 # matrix file
-write.table(PanCan_sig@featFitting[["sampleByComponent"]], file = paste0(output_prefix, '.matrix.txt'))
+write.table(PanCan_sig@featFitting[["sampleByComponent"]], file = paste0(output_prefix, '.SCmatrix.txt'))
 # matrix object
-saveRDS(PanCan_sig@featFitting[["sampleByComponent"]], file = paste0(output_prefix, '.matrix.rds'), compress = FALSE)
+saveRDS(PanCan_sig@featFitting[["sampleByComponent"]], file = paste0(output_prefix, '.SCmatrix.rds'), compress = FALSE)
 # object with full information (including activities, weights)
 saveRDS(PanCan_sig, file = paste0(output_prefix, '.signature.rds'), compress = FALSE)
 # simple heatmaps
@@ -47,3 +50,37 @@ dev.off()
 pdf(paste0(output_prefix,'.component.pdf'))
 plotSampleByComponent(PanCan_sig)
 dev.off()
+
+# apply cosine similarity to find the enriched signature for each sample
+## load the signature definition table for PanCancer signatures
+pan_mat_input <- snakemake@params[['def_SC']]
+pan_cancer_mat <- read.xlsx(pan_mat_input, colNames = TRUE, rowNames=TRUE)
+pan_cancer_mat <- t(pan_cancer_mat)
+pancan_sig <- colnames(pan_cancer_mat)
+
+# calculation
+SCmatrix <- PanCan_sig@featFitting[["sampleByComponent"]]
+SCmatrix <- t(SCmatrix)
+samples <- colnames(SCmatrix)
+cos_mat <- cbind(pan_cancer_mat, SCmatrix)
+cos_mat <- cosine(cos_mat)
+
+# extract the required sample-by-signature similarity matrix
+SSmatrix <- cos_mat[samples,]
+SSmatrix <- as.data.frame(SSmatrix) %>% select(all_of(pancan_sig))
+
+# find the most similar signature for each sample
+sample_by_signature <- apply(SSmatrix, 1, function(t) colnames(SSmatrix)[which.max(t)])
+sample_by_signature <- as.list(sample_by_signature)
+SSmatrix['enrich'] <- NA
+for (i in samples) {
+  sigID <- sample_by_signature[[i]]
+  SSmatrix[i, 'enrich'] <- sigID
+}
+
+# save the similarity SSmatrix
+SSmatrix['sample'] <- rownames(SSmatrix)
+SSmatrix <- SSmatrix %>% select('sample', 'enrich', 'CX1':'CX17')
+row.names(SSmatrix) <- 1:nrow(SSmatrix)
+write.table(SSmatrix, file = paste0(output_prefix,'.SSmatrix.tsv'), sep = '\t', row.names = FALSE, col.names = TRUE, quote = FALSE)
+saveRDS(SSmatrix, file = paste0(output_prefix, '.SSmatrix.rds'), compress = FALSE)
